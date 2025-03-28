0f536ad82b38a224f0961a20f08aecb34f8f6998
Ziomek921 <franek.grochowski@op.pl>
2025-03-28 01:09:31 +0100

    Initial commit

---
+++ Perceptron.py
@@ -0,0 +1,19 @@
+import random
+# unipolarny = przedział albo wybór (między) 0 i/a 1
+# dyskretny = 0 lub 1, boolean
+#input array/lista
+#waga array/lista
+#prog = round(float(random.random()),2)
+
+#przyjmuję wektor wejść i zwracam odpowiedź perceptrona
+#def compute():
+
+#przyjmuję wektor wejść i poprawną odpowiedź, i tu wsadzam regułe delta do modyfikacji progu
+#def learn():
+
+    #reguła delta
+
+
+#generator wag losowych
+waga = round(float(random.random()),2)
+print(waga)
